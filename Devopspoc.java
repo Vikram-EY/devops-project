f8fe2b03be74100b85b49e8fe25399763a205adc
FJ243HY <vikram.brahmachari@in.ey.com>
2021-02-16 19:51:44 +0530

    java sample code

---
+++ Devopspoc.java
@@ -0,0 +1,8 @@
+// Your First Program
+
+class Devopspoc {
+    public static void main(String[] args) {
+        System.out.println("Hello Team!, Good Morning..."); 
+        System.out.println("Sample DevOps pipeline demo");
+    }
+}
