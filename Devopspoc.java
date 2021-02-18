2ce07ab11e1d9af9f9c2bb5a80bf872d0757f174
Vikram-EY <78902543+Vikram-EY@users.noreply.github.com>
2021-02-18 21:23:23 +0530

    Update Devopspoc.java

--- Devopspoc.java
+++ Devopspoc.java
@@ -2,8 +2,7 @@
 
 class Devopspoc {
     public static void main(String[] args) {
-        System.out.println("Hello Team!, Good Morning..."); 
-        System.out.println("Sample DevOps pipeline demo");
+        System.out.println("Hello Team!, Good Morning...");
         System.out.println("Sample DevOps GIT/CI Demo");
     }
 }
